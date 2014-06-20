9e9a876f0b99c2f2cd6fe1ad56ce0db30a5ad346
Edward Price <ted.price@gmail.com>
2014-06-20 00:10:28 -0500

    Beginning vm tests and module

---
+++ vm_test.go
@@ -0,0 +1,57 @@
+package vm
+
+import (
+	"testing"
+)
+
+func TestMemorySize(t *testing.T) {
+	vm := NewVM()
+
+	exp := 4096
+	res := len(vm.memory)
+
+	if res != exp {
+		t.Error("vm.memory size is ", res, " expected ", exp)
+	}
+}
+
+func TestMaxStackFrames(t *testing.T) {
+	vm := NewVM()
+
+	exp := 40
+	res := len(vm.stack)
+
+	if res != exp {
+		t.Error("vm.stack size is ", res, " expected ", exp)
+	}
+}
+
+func TestRegisters(t *testing.T) {
+	vm := NewVM()
+
+	exp := 16
+	res := len(vm.V)
+
+	if res != exp {
+		t.Error("vm.V is ", res, " expected ", exp)
+	}
+}
+
+func TestI(t *testing.T) {
+	vm := NewVM()
+
+	exp := 0
+	res := vm.I
+
+	if res != exp {
+		t.Error("vm.I is ", res, " expected ", exp)
+	}
+}
+
+func TestNewVM(t *testing.T) {
+	vm := NewVM()
+
+	if vm.pc != 0x200 {
+		t.Error("vm not defaulted with pc 0x200!")
+	}
+}

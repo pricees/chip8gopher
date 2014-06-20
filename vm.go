ec81f180cb5b671ce89853486679bf51ab60fbf1
Edward Price <ted.price@gmail.com>
2014-06-20 00:21:59 -0500

    Removing hard tabs

--- vm.go
+++ vm.go
@@ -3,26 +3,26 @@
 package vm
 
 type Op struct {
-	opcode  int
-	operand int
+  opcode  int
+  operand int
 }
 
 type VM struct {
-	display  Display
-	cpuSpeed int     // hertz
-	stack    [40]int // TODO: Find size of Chip-8 stack
+  display  Display
+  cpuSpeed int     // hertz
+  stack    [40]int // TODO: Find size of Chip-8 stack
 
-	// 4KB of memory
-	memory [4096]uint8
+  // 4KB of memory
+  memory [4096]uint8
 
-	// Registers
-	V [16]uint8 // Vx: 16 generate purpose 8-bit registers
-	I int       // 16-bit register, for storing addresses
+  // Registers
+  V [16]uint8 // Vx: 16 generate purpose 8-bit registers
+  I int       // 16-bit register, for storing addresses
 
-	// Program counter, start at 0x200
-	pc int
+  // Program counter, start at 0x200
+  pc int
 }
 
 func NewVM() *VM {
-	return &VM{pc: 0x200}
+  return &VM{pc: 0x200}
 }
--- vm_test.go
+++ vm_test.go
@@ -2,57 +2,57 @@
 package vm
 
 import (
-	"testing"
+  "testing"
 )
 
 func TestMemorySize(t *testing.T) {
-	vm := NewVM()
+  vm := NewVM()
 
-	exp := 4096
-	res := len(vm.memory)
+  exp := 4096
+  res := len(vm.memory)
 
-	if res != exp {
-		t.Error("vm.memory size is ", res, " expected ", exp)
-	}
+  if res != exp {
+    t.Error("vm.memory size is ", res, " expected ", exp)
+  }
 }
 
 func TestMaxStackFrames(t *testing.T) {
-	vm := NewVM()
+  vm := NewVM()
 
-	exp := 40
-	res := len(vm.stack)
+  exp := 40
+  res := len(vm.stack)
 
-	if res != exp {
-		t.Error("vm.stack size is ", res, " expected ", exp)
-	}
+  if res != exp {
+    t.Error("vm.stack size is ", res, " expected ", exp)
+  }
 }
 
 func TestRegisters(t *testing.T) {
-	vm := NewVM()
+  vm := NewVM()
 
-	exp := 16
-	res := len(vm.V)
+  exp := 16
+  res := len(vm.V)
 
-	if res != exp {
-		t.Error("vm.V is ", res, " expected ", exp)
-	}
+  if res != exp {
+    t.Error("vm.V is ", res, " expected ", exp)
+  }
 }
 
 func TestI(t *testing.T) {
-	vm := NewVM()
+  vm := NewVM()
 
-	exp := 0
-	res := vm.I
+  exp := 0
+  res := vm.I
 
-	if res != exp {
-		t.Error("vm.I is ", res, " expected ", exp)
-	}
+  if res != exp {
+    t.Error("vm.I is ", res, " expected ", exp)
+  }
 }
 
 func TestNewVM(t *testing.T) {
-	vm := NewVM()
+  vm := NewVM()
 
-	if vm.pc != 0x200 {
-		t.Error("vm not defaulted with pc 0x200!")
-	}
+  if vm.pc != 0x200 {
+    t.Error("vm not defaulted with pc 0x200!")
+  }
 }

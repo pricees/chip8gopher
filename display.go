cebf6bd9a86e1fbba3ff026ced55ba9d32d6ab5c
Edward Price <ted.price@gmail.com>
2014-06-21 17:47:42 -0500

    Adding Display methods. Look ma, no tests

--- display.go
+++ display.go
@@ -1,8 +1,53 @@
 package vm
 
+const BLANK = "_"
+
 type Display struct {
+	canvas [][]string
+	pixels [][]string
+
+	// Display dimensions
+	width  int
+	height int
+
+	xScale int
+	yScale int
 }
 
-func (dm *Display) XorPixel(x int, y int) bool {
-	return true
+func NewDisplay() *Display {
+	return &Display{canvas: [][]string{}, width: 64, height: 32}
+}
+
+func (display *Display) Clear() {
+	for i := 0; i < display.width; i++ {
+		for j := 0; j < display.height; j++ {
+			display.canvas[i][j] = BLANK
+		}
+	}
+}
+
+func (display *Display) XorPixel(x int, y int) bool {
+	// Wrap around vertically
+	if x > display.width {
+		x -= display.width
+	} else if x < 0 {
+		x += display.width
+	}
+
+	// Wrap around horizontally
+	if y > display.height {
+		y -= display.height
+	} else if y < 0 {
+		y += display.height
+	}
+
+	// Set the pixel state
+	active := display.canvas[x][y] == BLANK
+	if active {
+		// Javascript was: var active = this.canvas[x][y] ^= 1
+		display.canvas[x][y] = "*"
+	} else {
+		display.canvas[x][y] = " "
+	}
+	return active
 }
